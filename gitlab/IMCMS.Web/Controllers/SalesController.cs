22397388a35ff029e253083fee8577f8907195b5
mib1092 <mazurenko.ihor@gmail.com>
2017-11-07 00:33:49 +0200

    mib

---
+++ gitlab/IMCMS.Web/Controllers/SalesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMCMS.Web.Controllers
+{
+  [RoutePrefix("sales")]
+  public class SalesController : BaseController
+  {
+    [Route("")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+    [Route("detail")]
+    public ActionResult Detail()
+    {
+      return View();
+    }
+  }
+}
